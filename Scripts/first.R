e6a60a60fb51e91102fa5161a3895c6574e7692a
gauravmeena0708 <gauravmeena0708@gmail.com>
2022-07-03 23:16:04 +0530

    adding snippet in first

--- Scripts/first.R
+++ Scripts/first.R
@@ -6,4 +6,20 @@
 #logical
 #1L is integer specifically
 #Inf is infinity
+#NaN is not a number
 
+#attributes
+#name, dimnames, dimensions, class, length
+x <- c(T,F)
+x
+y<- c(1+1i, 2+3i)
+y
+
+#conversion
+as.character(y)
+z<-as.integer(y)
+
+
+#list can have items of diff types
+a<- list(1, "String")
+a
